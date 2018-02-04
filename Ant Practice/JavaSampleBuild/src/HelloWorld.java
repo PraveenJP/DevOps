0413517686237d7cd7004d268257251011219e06
praveenjp <roadizpraveen@gmail.com>
2018-02-04 18:25:43 +0530

    Ant practice and notes added

---
+++ Ant Practice/JavaSampleBuild/src/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.*;
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Logger log = Logger.getLogger(HelloWorld.class.getName());
+	}
+}
